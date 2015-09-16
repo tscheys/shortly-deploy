92fe47e901de73cae799a7f79bbac8722217f9f1
tscheys <tim.scheys@gmail.com>
2015-09-16 14:48:05 -0700

    Changed IP to 0.0.0.0

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ var mongoUrl = null;
 if (process.env.NODE_ENV === 'production') {
   port = 80;
   mongoUrl = 'mongodb://theBuffetDB:XAbbOqi7mdlEs4oagW6taZLFB6ADVccJcq.UoK.yQaE-@ds040888.mongolab.com:40888/theBuffetDB'
+  url = "0.0.0.0"
 }
 else {
   port = 4568
   mongoUrl = 'mongodb://localhost/shortly'
+  url = "127.0.0.1"
 }
 
 mongoose.connect(mongoUrl);
 
-app.listen(port);
+app.listen(port, url);
 
 console.log('Server now listening on port ' + port);
