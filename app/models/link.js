3fb89ccd6ae1fa7478f624aabda390644e7f7fe4
tscheys <tim.scheys@gmail.com>
2015-09-17 18:48:36 -0700

    Advanced

--- app/models/link.js
+++ app/models/link.js
@@ -2,26 +2,14 @@ var crypto = require('crypto');
 var mongoose = require('mongoose');
 
 var LinkSchema = new mongoose.Schema({
-  visits: 'string',
+  visits: {
+    type: 'number',
+    default: 0
+  },
   url: 'string', 
   title: 'string',
   code: 'string'
 });
 var Link = mongoose.model('Link', LinkSchema);
-
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function(){
-//     this.on('creating', function(model, attrs, options){
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
 
 module.exports = Link;
